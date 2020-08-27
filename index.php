b7a96cf97b39099b5c63fdb3f37ee825f7b39792
Cphil31 <cecilephilippe31@gmail.com>
2020-08-27 21:00:04 +0200

    lien http

--- index.php
+++ index.php
@@ -2,3 +2,8 @@
 //si l'utilisateur est entrain d'utiliser un smartphone/tablette pendant la navigation sur votre site 
 $useragent=$_SERVER['HTTP_USER_AGENT'];
 var_dump($useragent);
+
+/*
+lien http
+http://www.codeurjava.com/2017/01/php-detecter-si-un-terminal-est-un-mobile-tablette.html
+*/
