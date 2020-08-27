68e738b31af6fa4b6211e2be40df7448be6a42aa
Cphil31 <cecilephilippe31@gmail.com>
2020-08-27 21:18:40 +0200

    autre lien

--- index.php
+++ index.php
@@ -11,3 +11,7 @@ http://www.codeurjava.com/2017/01/php-detecter-si-un-terminal-est-un-mobile-tabl
 // adresse ip de la machine 
 $server=$_SERVER['REMOTE_ADDR'];
 var_dump($server);
+/*
+lien http 
+http://devblog.fr/2017/08/22/connaitre-ladresse-ip-dun-visiteur-avec-php/#:~:text=Pour%20acc%C3%A9der%20%C3%A0%20cette%20information,SERVER%5B'REMOTE_ADDR'%5D.
+*/
